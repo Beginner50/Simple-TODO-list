3887503a92dab502d88a8c45980293493c8016b7
prashant <prashant@debian-BULLSEYE-live-builder-AMD64>
2023-04-12 22:00:50 +0400

    V.0.2 TODO List

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/home/prashant/Desktop/pygame/.venv/bin/python3
+import pygame
+from renderer import Renderer
+import os
+
+
+class Main:
+    def __init__(self) -> None:
+        posx, posy = 1000, 400
+        os.environ["SDL_VIDEO_WINDOW_POS"] = f"{posx},{posy}"
+
+        pygame.init()
+
+        self.window = pygame.display.set_mode((400, 400))
+        pygame.display.set_caption("Todo list")
+
+        self.renderer = Renderer()
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            dt = self.clock.tick() / 1000
+
+            self.renderer.run(dt)
+
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
